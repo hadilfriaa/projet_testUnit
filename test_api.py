99f71a74835e1c7393fcee6b3daea598accd1c2b
hadilfriaa <hadil.friaa@ynov.com>
2022-05-12 23:17:07 +0200

    up

--- test_api.py
+++ test_api.py
@@ -13,17 +13,17 @@ class TestApi(TestCase):
         self.url = 'http://127.0.0.1:8000/docs'
         print("oui")
 
-    #def test_create(self):
-    #    user = requests.post('http://127.0.0.1:8000/', json = {"name": "test1","email": "test@test.com","password": "123456"})
-    #    self.assertEqual(user.status_code, 200)
+    def test_create(self):
+        user = requests.post('http://127.0.0.1:8000/', json = {"name": "test1","email": "test@test.com","password": "123456"})
+        self.assertEqual(user.status_code, 200)
     
-    #def test_getAll(self):
-    #    users = requests.get('http://127.0.0.1:8000/').json()
-    #    self.assertNotEqual(len(users), 0)
+    def test_getAll(self):
+        users = requests.get('http://127.0.0.1:8000/').json()
+        self.assertNotEqual(len(users), 0)
 
-    #def test_updateUser(self):
-    #    user = requests.put('http://127.0.0.1:8000/6277ca2a9410356d6bde4792', json = {"name": "test update","email": "test@test.com","password": "123456"})
-    #    self.assertEqual(user.status_code, 200)
+    def test_updateUser(self):
+        user = requests.put('http://127.0.0.1:8000/6277ca2a9410356d6bde4792', json = {"name": "test update","email": "test@test.com","password": "123456"})
+        self.assertEqual(user.status_code, 200)
 
     def test_deleteUser(self):
         user = requests.delete('http://127.0.0.1:8000/6277c5136c00629f16d2b394')
